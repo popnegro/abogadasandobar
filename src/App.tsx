e7ef54d210899102235037d78c39a143d5a9a7a8
luisgrasso <grasso.luis@gmail.com>
2026-09-06 19:24:06 -0300

    fix(seo): redirect legacy experiencia route

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ function PageShell() {
           <Route path="/inicio" element={<HomeSection setActiveTab={setActiveTab} onRequestConsultation={() => handleRequestConsultation()} />} />
           <Route path="/servicios-abogacia-mendoza" element={<ServicesSection setActiveTab={setActiveTab} onRequestConsultation={(area) => handleRequestConsultation(area)} />} />
           <Route path="/abogada-penalista-mendoza" element={<ExperienceSection setActiveTab={setActiveTab} onOpenConsultationModal={() => handleRequestConsultation()} />} />
+          <Route path="/experiencia" element={<Navigate to="/abogada-penalista-mendoza" replace />} />
           <Route path="/nuestro-metodo" element={<MethodSection setActiveTab={setActiveTab} onOpenConsultationModal={() => handleRequestConsultation()} />} />
           <Route path="/preguntas-frecuentes" element={<FAQSection setActiveTab={setActiveTab} onOpenConsultationModal={() => handleRequestConsultation()} />} />
           <Route path="/contacto" element={<ContactSection setActiveTab={setActiveTab} initialPracticeArea={contactPracticeArea} />} />
